f2a4200a7ee99c72754072e0932a3f4598b74b98
Vipershroom <undertalefan812@gmail.com>
2023-01-03 20:26:26 -0500

    Add get_page_range method

--- LogsTf/logs.py
+++ LogsTf/logs.py
@@ -7,12 +7,19 @@ class LogsTf:
     
     # Takes in player_id and formats it into a link.
     def __init__(self, player_id):
-        self.player_id = f"{url}/profile/{player_id}"
+        self.player_id = player_id
         
     # Gets latest log from the user
     def get_latest_log(self):
-        source = requests.get(self.player_id).text
+        source = requests.get(f"{url}/{self.player_id}").text
         return f"{url}/{BeautifulSoup(source, 'lxml').find('td').a['href']}"
     
+    """Takes in the amount of logs the user wants. Then
+    calculates how many pages would be needed to get that many logs and
+    returns them in a list."""
+    def get_page_range(self, page_range):
+        num = ceil(page_range / 25)
+        return [f"{url}/profile/{self.player_id}?p={i}" for i in range(1,num+1)]
+    
 n = LogsTf("76561198322448472")
-print(n.get_latest_log())
+print(n.get_page_range(75))
