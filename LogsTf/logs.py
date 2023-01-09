9d0db2431c529d110f43e9cbc2fa44cd6b2dbdad
Vipershroom <undertalefan812@gmail.com>
2023-01-08 19:51:33 -0500

    Add get_logs method

--- LogsTf/logs.py
+++ LogsTf/logs.py
@@ -21,7 +21,27 @@ class LogsTf:
         num = ceil(page_range / 25)
         return [f"{url}/profile/{self.player_id}?p={i}" for i in range(1,num+1)]
     
-
+    """Returns the amount of logs the user wants
+    in the form of a list. Logs are returned as links."""
+    def get_logs(self, amount: int) -> list:
+        
+        # Gets the amount of pages to loop over.
+        page_links = self.__get_page_range(amount)
+        
+        log_list = []
+        
+        # Makes sure the log count doesn't go over the specified amount
+        counter = 0
+        
+        for links in page_links:
+            for logs in BeautifulSoup(requests.get(links).text, 'lxml').find_all('td'):
+                if logs.a != None and counter < amount:
+                    log_list.append(f"{url}/{logs.a['href']}")
+                    counter += 1
+        return log_list            
+            
+            
+    
     
 n = LogsTf("76561198322448472")
-print(n.get_page_range(75))
+print(n.get_logs(26))
