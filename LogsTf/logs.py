51a6311b7bee5020b1f49180b8ef868a37c1b7b2
Vipershroom <undertalefan812@gmail.com>
2023-01-09 15:43:22 -0500

    Add get_data andd store_data methods

--- LogsTf/logs.py
+++ LogsTf/logs.py
@@ -39,9 +39,34 @@ class LogsTf:
                     log_list.append(f"{url}/{logs.a['href']}")
                     counter += 1
         return log_list            
+
+    # Gets the data of a single log and returns a dictionary
+    def get_data(self, log_link: str) -> dict:
+        
+        content = BeautifulSoup(requests.get(log_link).text, 'lxml').find('tr', id=f'player_{self.player_id}')
+        
+        # Stores all the data into a dictionary and returns it
+        return self.__store_data([content.find_all('td', class_=None)[i].text for i in range(13)])
+    
+    def __store_data(self, log):
+        return {
+            "Kills": log[0],
+            "Assists": log[1],
+            "Deaths": log[2],
+            "Damage": log[3],
+            "Damage / Minute": log[4],
+            "Kills and Assists / Deaths": log[5],
+            "Kills / Deaths": log[6],
+            "Damage Taken": log[7],
+            "Damage Taken / Minute": log[8],
+            "Health pickups": log[9],
+            "Backstabs": log[10],
+            "Headshots": log[11],
+            "Capture Points Captures": log[12],
+        }
             
             
     
     
 n = LogsTf("76561198322448472")
-print(n.get_logs(26))
+print(n.get_data(n.get_logs(26)[0]))
